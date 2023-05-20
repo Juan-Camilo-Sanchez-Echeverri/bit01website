d7aaae0f32dfbd8671d4ae06665a7ed86c880e15
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2023-05-19 21:34:26 -0500

    Se organiza el bug de que el objecto no llenaba al enviar

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,16 @@
 const $form = document.getElementById('form');
-
-const $nombreApellido = document
-  .getElementById('nombreapellido')
-  .value.toString();
-const $correo = document.getElementById('correo').value;
-const $telefono = document.getElementById('telefono').value;
-const $mensaje = document.getElementById('mensaje').value;
-const $contacto = document.getElementsByName('contacto');
-const $novedades = document.getElementById('novedades').checked;
 const $gracias = document.getElementById('gracias');
 
-const $nombre = document.getElementById('nombre');
-const correo = document.getElementById('correo');
-
-function valueInput() {
-  let info;
-  for (let i = 0; i < $contacto.length; i++) {
-    if ($contacto[i].checked) {
-      info = $contacto[i].value;
-      break;
-    }
-  }
-  return info;
-}
-
-let infoContacto = valueInput();
-
-let informacion = {
-  nombreCompleto: $nombreApellido,
-  correo: $correo,
-  telefono: $telefono,
-  mensaje: $mensaje,
-  contacto: infoContacto,
-};
-
-$form.addEventListener('submit', () => {
+$form.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  const $nombreApellido = document.getElementById('nombreapellido').value;
+  const $correo = document.getElementById('correo').value;
+  const $telefono = document.getElementById('telefono').value;
+  const $mensaje = document.getElementById('mensaje').value;
+  const $contacto = document.getElementsByName('contacto');
+
+  let infoContacto;
   for (let i = 0; i < $contacto.length; i++) {
     if ($contacto[i].checked) {
       infoContacto = $contacto[i].value;
@@ -44,11 +18,18 @@ $form.addEventListener('submit', () => {
     }
   }
 
+  let informacion = {
+    nombreCompleto: $nombreApellido,
+    correo: $correo,
+    telefono: $telefono,
+    mensaje: $mensaje,
+    contacto: infoContacto,
+  };
+
   console.log(informacion);
 });
 
 function cambioEstilos() {
   $form.style.display = 'none';
-  $gracias.style.display = 'show';
   $gracias.style.display = 'flex';
 }
