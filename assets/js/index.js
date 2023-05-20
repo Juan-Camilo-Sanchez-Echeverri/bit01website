1c62bdd5c4f97ae6e96569277e0d2db735b44bce
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2023-05-19 21:05:40 -0500

    Agregando js al proyecto

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+const $form = document.getElementById('form');
+
+const $nombreApellido = document
+  .getElementById('nombreapellido')
+  .value.toString();
+const $correo = document.getElementById('correo').value;
+const $telefono = document.getElementById('telefono').value;
+const $mensaje = document.getElementById('mensaje').value;
+const $contacto = document.getElementsByName('contacto');
+const $novedades = document.getElementById('novedades').checked;
+const $gracias = document.getElementById('gracias');
+
+const $nombre = document.getElementById('nombre');
+const correo = document.getElementById('correo');
+
+//!Corregir Bug
+function valueInput() {
+  let infoContacto = '';
+  for (let i = 0; i < $contacto.length; i++) {
+    if ($contacto[i].checked) {
+      infoContacto = $contacto[i].value;
+      break;
+    }
+  }
+}
+
+let infoContacto = valueInput();
+
+let informacion = {
+  nombreCompleto: $nombreApellido,
+  correo: $correo,
+  telefono: $telefono,
+  mensaje: $mensaje,
+  contacto: infoContacto,
+};
+
+$form.addEventListener('submit', () => {
+  event.preventDefault();
+
+  for (let i = 0; i < $contacto.length; i++) {
+    if ($contacto[i].checked) {
+      infoContacto = $contacto[i].value;
+      break;
+    }
+  }
+
+  console.log(informacion);
+});
+
+function cambioEstilos() {
+  $form.style.display = 'none';
+  $gracias.style.display = 'show';
+  $gracias.style.display = 'flex';
+}
